2e5371470a3d49f9b73d664ce8f86d2383d4d272
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-08-07 21:44:59 +1000

    created an sql database and used to it to generate html using jinja

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+import sqlite3
+
+
+def get_db_connection():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    return connection
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    connection = get_db_connection()
+    posts = connection.execute("SELECT * FROM posts").fetchall()
+    connection.close()
+    return render_template('index.html', posts=posts)
